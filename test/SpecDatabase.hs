1d864baa9e8d595db7ee5bf803b036e22bbbd7e0
nico heimann <kksnicoh@nicos-MBP.fritz.box>
2020-04-18 16:26:07 +0200

    improved database test cases

--- test/SpecDatabase.hs
+++ test/SpecDatabase.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module SpecDatabase where
 
 import           Test.Hspec
@@ -6,6 +7,9 @@ import           Core.Database.Env
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.Types
                                                 ( Query(Query) )
+import           Control.Exception              ( try )
+import           Control.Exception.Base         ( SomeException )
+import           GHC.Int                        ( Int64 )
 
 withDatabaseMigrated
   :: (HasDbConnection e, HasDbSchema e)
@@ -14,5 +18,12 @@ withDatabaseMigrated
   -> Expectation
 withDatabaseMigrated env spec = do
   let conn = getDbConnection env
-  forM_ (Query <$> getDbSchema env) $ \q -> execute conn q ()
-  spec conn
+  forM_ (Query <$> getDbSchema env) (tryIgnore conn) >> spec conn
+ where
+  tryIgnore c q = try (execute c q ()) >>= printSchemaError
+  printSchemaError :: Either SomeException Int64 -> IO ()
+  printSchemaError = \case
+    Left err -> do
+      putStrLn "[WARN] withDatabaseMigrated error during schema execution"
+      print err
+    _ -> return ()
