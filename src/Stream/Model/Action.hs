d7238b1ac568e00d88d5ef29efed695e3aa7a022
nico heimann <keksnicoh@googlemail.com>
2020-05-04 14:52:36 +0200

    improved websockets action

--- src/Stream/Model/Action.hs
+++ src/Stream/Model/Action.hs
@@ -5,9 +5,15 @@ module Stream.Model.Action where
 import           Stream.Model.LightState        ( LightState(..) )
 import           Data.Aeson
 
-newtype Action = LightAction LightState
+data Action
+  = LightAction LightState 
+  | LightChangedAction LightState 
+  | PingAction
   deriving(Show, Eq)
 
 instance ToJSON Action where
   toJSON (LightAction lightState) =
     object ["action" .= String "light", "data" .= toJSON lightState]
+  toJSON (LightChangedAction lightState) =
+    object ["action" .= String "light_changed", "data" .= toJSON lightState]
+  toJSON PingAction = object ["action" .= String "ping"]
--- src/Stream/Service/Action.hs
+++ src/Stream/Service/Action.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Stream.Service.Action where
 
 import qualified Core.State.Repository.State   as CS
@@ -5,49 +6,109 @@ import           Control.Concurrent             ( threadDelay )
 import           Control.Monad.IO.Class         ( MonadIO
                                                 , liftIO
                                                 )
-import           Data.Foldable                  ( forM_ )
-
 import           Network.WebSockets             ( Connection
-                                                , forkPingThread
                                                 , sendTextData
+                                                , receiveData
                                                 )
-
-
-
-import           Core.State.Env                 ( HasState(..) )
-import           Control.Monad.Reader           ( MonadReader
+import           Control.Monad.Reader           ( reader
+                                                , MonadReader
                                                 , when
-                                                )
-import           Data.IORef                     ( newIORef
-                                                , readIORef
-                                                , atomicModifyIORef'
                                                 )
 import           Data.Aeson                     ( encode )
 import           Stream.Model.LightState
 import           Stream.Model.Action
+import qualified Dependencies                  as D
+import           Data.Time
+import qualified Data.ByteString               as BS
+import           Core.State.Model.State
+import           Control.Monad.State.Strict     ( runStateT
+                                                , modify'
+                                                , StateT
+                                                , gets
+                                                )
+
+data ActionEnv
+  = ActionEnv
+    { lastLightStateC :: LightState
+    , pingTimeC :: UTCTime
+    , lightTimeC :: UTCTime
+    , connectionC :: Connection
+    , dispatchTimeC :: UTCTime
+    , dispatchStateC :: State
+    }
+
+data ActionResult = Continue | Exit
 
 streamData
-  :: (MonadIO m, MonadReader e m, HasState e)
+  :: (MonadIO m, MonadReader e m, D.HasCurrentTime e)
   => CS.GetState m
   -> Connection
   -> m ()
-streamData getCurrentState c = do
+streamData getCurrentState connection = do
+  liftIO $ putStrLn "new client"
+  currentTime  <- reader D.getCurrentTime >>= liftIO
   currentState <- getCurrentState
-  let lightState = fromState currentState
-  lastStateIoRef <- liftIO $ newIORef lightState
+  run ActionEnv { lastLightStateC = fromState currentState
+                , pingTimeC       = currentTime
+                , lightTimeC      = addUTCTime (-300) currentTime
+                , connectionC      = connection
+                , dispatchTimeC    = currentTime
+                , dispatchStateC   = currentState
+                }
+ where
+  run env = do
+    result <- liftIO $ runStateT actions env
+    case result of
+      (Continue, env) -> do
+        liftIO $ threadDelay 100000
+        currentState <- getCurrentState
+        currentTime  <- reader D.getCurrentTime >>= liftIO
+        run env { dispatchStateC = currentState, dispatchTimeC = currentTime }
+      (Exit, a) -> liftIO $ putStrLn "exit..."
 
-  liftIO $ do
-    putStrLn "New Stream Client..."
-    sendTextData c (encode $ LightAction lightState)
-    forkPingThread c 10
+actions :: StateT ActionEnv IO ActionResult
+actions = do
+  switchUpdatedAction
+  switchAction
+  pingPongAction
 
-  forM_ [1 ..] $ \i -> do
+switchUpdatedAction :: StateT ActionEnv IO ()
+switchUpdatedAction = do
+  state          <- fromState <$> gets dispatchStateC
+  lastLightState <- gets lastLightStateC
+  connection     <- gets connectionC
+  when (state /= lastLightState) $ do
+    liftIO $ sendTextData connection (encode $ LightChangedAction state)
+    modify' (\s -> s { lastLightStateC = state, lightTimeC = dispatchTimeC s })
+    liftIO $ putStrLn "[light_changed] state send"
 
-    state <- fromState <$> getCurrentState
-    liftIO $ do
-      lastState <- readIORef lastStateIoRef
-      when (state /= lastState) $ do
-        sendTextData c (encode $ LightAction state)
-        atomicModifyIORef' lastStateIoRef $ const (state, ())
+switchAction :: StateT ActionEnv IO ()
+switchAction = do
+  now       <- gets dispatchTimeC
+  lightTime <- gets lightTimeC
+  when (diffUTCTime now lightTime > 60) $ do
+    state      <- fromState <$> gets dispatchStateC
+    connection <- gets connectionC
+    liftIO $ sendTextData connection (encode $ LightAction state)
+    modify' (\s -> s { lastLightStateC = state, lightTimeC = now })
+    liftIO $ putStrLn "[light] state send"
 
-      threadDelay 100000
+pingPongAction :: StateT ActionEnv IO ActionResult
+pingPongAction = do
+  now      <- gets dispatchTimeC
+  pingTime <- gets pingTimeC
+  if diffUTCTime now pingTime > 10
+    then do
+      connection     <- gets connectionC
+      pingPongResult <- liftIO $ do
+        sendTextData connection (encode PingAction)
+        receiveData connection :: IO BS.ByteString
+      modify' (\s -> s { pingTimeC = now })
+      if pingPongResult == "pong"
+        then do
+          liftIO $ putStrLn "[ping] pong!"
+          return Continue
+        else do
+          liftIO $ putStrLn "[ping] no pong, terminating..."
+          return Exit
+    else return Continue
