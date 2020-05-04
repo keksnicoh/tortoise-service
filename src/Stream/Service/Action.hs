a6f158c73aa56b2be7930250e4b9a8e1a16fb126
nico heimann <keksnicoh@googlemail.com>
2020-05-04 18:49:39 +0200

    refactor: simplified Stream.Service.Action

--- src/Stream/Service/Action.hs
+++ src/Stream/Service/Action.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Stream.Service.Action where
@@ -15,14 +16,17 @@ import           Control.Monad.Reader           ( reader
                                                 , MonadReader
                                                 , when
                                                 )
-import           Data.Aeson                     ( encode )
+import           Data.Aeson                     ( ToJSON
+                                                , encode
+                                                )
 import           Stream.Model.LightState
 import           Stream.Model.Action
 import qualified Dependencies                  as D
 import           Data.Time
-import qualified Data.ByteString               as BS
+import qualified Data.ByteString.Lazy          as LBS
 import           Core.State.Model.State
-import           Control.Monad.State.Strict     ( runStateT
+import           Control.Monad.State.Strict     ( MonadState
+                                                , runStateT
                                                 , modify'
                                                 , StateT
                                                 , gets
@@ -33,13 +37,25 @@ data ActionEnv
     { lastLightStateC :: LightState
     , pingTimeC :: UTCTime
     , lightTimeC :: UTCTime
-    , connectionC :: Connection
     , dispatchTimeC :: UTCTime
     , dispatchStateC :: State
     , webcamRequestDateC :: UTCTime
+    , sendC :: LBS.ByteString -> IO ()
+    , receiveC :: IO LBS.ByteString
     }
 
 data ActionResult = Continue | Exit
+
+debug :: MonadIO m => String -> m ()
+debug msg = liftIO (putStrLn msg)
+
+send :: (MonadState ActionEnv m, MonadIO m, ToJSON a) => a -> m ()
+send f = do
+  sendTextData <- gets sendC
+  liftIO . sendTextData . encode $ f
+
+receive :: (MonadState ActionEnv m, MonadIO m) => m LBS.ByteString
+receive = gets receiveC >>= liftIO
 
 streamData
   :: (MonadIO m, MonadReader e m, D.HasCurrentTime e)
@@ -47,16 +63,17 @@ streamData
   -> Connection
   -> m ()
 streamData getCurrentState connection = do
-  liftIO $ putStrLn "new client"
+  debug "new client"
   currentTime  <- reader D.getCurrentTime >>= liftIO
   currentState <- getCurrentState
   run ActionEnv { lastLightStateC    = fromState currentState
                 , pingTimeC          = currentTime
                 , lightTimeC         = addUTCTime (-300) currentTime
-                , connectionC        = connection
                 , dispatchTimeC      = currentTime
                 , dispatchStateC     = currentState
                 , webcamRequestDateC = currentTime
+                , sendC              = sendTextData connection
+                , receiveC           = receiveData connection
                 }
  where
   run env = do
@@ -67,7 +84,7 @@ streamData getCurrentState connection = do
         currentState <- getCurrentState
         currentTime  <- reader D.getCurrentTime >>= liftIO
         run env { dispatchStateC = currentState, dispatchTimeC = currentTime }
-      (Exit, a) -> liftIO $ putStrLn "exit..."
+      (Exit, a) -> debug "exit..."
 
 actions :: StateT ActionEnv IO ActionResult
 actions = do
@@ -80,22 +97,26 @@ switchUpdatedAction :: StateT ActionEnv IO ()
 switchUpdatedAction = do
   state          <- fromState <$> gets dispatchStateC
   lastLightState <- gets lastLightStateC
-  connection     <- gets connectionC
   when (state /= lastLightState) $ do
-    liftIO $ sendTextData connection (encode $ LightChangedAction state)
+    send (LightChangedAction state)
+    updateState state
+    debug "[light_changed] state send"
+ where
+  updateState state =
     modify' (\s -> s { lastLightStateC = state, lightTimeC = dispatchTimeC s })
-    liftIO $ putStrLn "[light_changed] state send"
 
 switchAction :: StateT ActionEnv IO ()
 switchAction = do
-  now       <- gets dispatchTimeC
-  lightTime <- gets lightTimeC
-  when (diffUTCTime now lightTime > 60) $ do
-    state      <- fromState <$> gets dispatchStateC
-    connection <- gets connectionC
-    liftIO $ sendTextData connection (encode $ LightAction state)
+  now      <- gets dispatchTimeC
+  timeDiff <- diffUTCTime now <$> gets lightTimeC
+  when (timeDiff > 60) $ do
+    state <- fromState <$> gets dispatchStateC
+    send (LightAction state)
+    updateState state now
+    debug "[light] state send"
+ where
+  updateState state now =
     modify' (\s -> s { lastLightStateC = state, lightTimeC = now })
-    liftIO $ putStrLn "[light] state send"
 
 webcamRequestAction :: StateT ActionEnv IO ()
 webcamRequestAction = do
@@ -103,27 +124,25 @@ webcamRequestAction = do
   webcamRequest <$> gets dispatchStateC >>= \case
     Nothing   -> return ()
     Just date -> when (webcamRequestDate < date) $ do
-      connection <- gets connectionC
-      liftIO $ sendTextData connection (encode WebcamAction)
-      modify' (\s -> s { webcamRequestDateC = dispatchTimeC s })
-      liftIO $ putStrLn "[webcam] requested"
+      send WebcamAction
+      updateState
+      debug "[webcam] requested"
+ where
+  updateState = modify' (\s -> s { webcamRequestDateC = dispatchTimeC s })
 
 pingPongAction :: StateT ActionEnv IO ActionResult
 pingPongAction = do
   now      <- gets dispatchTimeC
-  pingTime <- gets pingTimeC
-  if diffUTCTime now pingTime > 10
+  timeDiff <- diffUTCTime now <$> gets pingTimeC
+  if timeDiff > 10
     then do
-      connection     <- gets connectionC
-      pingPongResult <- liftIO $ do
-        sendTextData connection (encode PingAction)
-        receiveData connection :: IO BS.ByteString
-      modify' (\s -> s { pingTimeC = now })
-      if pingPongResult == "pong"
+      pingPongResponse <- send PingAction >> receive
+      if pingPongResponse == "pong"
         then do
-          liftIO $ putStrLn "[ping] pong!"
+          modify' (\s -> s { pingTimeC = now })
+          debug "[ping] pong!"
           return Continue
         else do
-          liftIO $ putStrLn "[ping] no pong, terminating..."
+          debug "[ping] no pong, terminating..."
           return Exit
     else return Continue
